7231c006c6e413bd4320c20a51a58b5350b101d3
Hiep Hoang <32895394+hoanghiep1992@users.noreply.github.com>
2021-04-27 15:37:48 +0700

    update

--- receiver.js
+++ receiver.js
@@ -3,8 +3,4 @@ const context = cast.framework.CastReceiverContext.getInstance();
 // Update style using javascript
 let playerElement = document.getElementsByTagName("cast-media-player")[0];
 playerElement.style.setProperty('--splash-image', 'url("http://some/other/image.png")');
-
-
-context.setBrowseContent(BrowseContent);
-
 context.start();
